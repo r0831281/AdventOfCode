b0aa017d0a4a94b84f3c52fc2bb7d19a8296f955
Jonas <r0831281@student.thomasmore.be>
2023-12-01 19:49:39 +0100

    stuck

--- day0/puzzly.py
+++ day0/puzzly.py
@@ -1,3 +1,4 @@
+import re
 examples = []
 
 
@@ -5,20 +6,61 @@ examples = []
 with open('input.txt', 'r') as f:
     for line in f:
         examples.append(line.strip())
-
-    print(examples)     
+   
         
 
+def find_written_numbers(text):
+    # Dictionary mapping written numbers to numeric values
+    word_to_num = {
+        'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+        'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10
+    }
+
+    # Sort the dictionary by the length of the keys in descending order
+    sorted_word_to_num = dict(sorted(word_to_num.items(), key=lambda x: len(x[0]), reverse=True))
+
+    # Initialize an empty list to store the numeric values
+    numeric_values = []
+
+    # Iterate over the sorted keys and find matches in the input text
+    for word in sorted_word_to_num.keys():
+        start = 0
+        while True:
+            match_start = text.lower().find(word, start)
+            if match_start == -1:
+                break
+
+            match_end = match_start + len(word)
+            numeric_values.append((word, sorted_word_to_num[word]))
+            start = match_end
+
+    return numeric_values
 
 def solve(examples):
     out = []
     final = 0
     for example in examples:
         data = []
+        rest = []
         for char in example:
             if char.isdigit():
                 integer = int(char)
-                data.append(integer)   
+                data.append(integer)
+            else:
+                rest.append(char)
+                
+        rest = "".join(rest)
+        print(rest)
+        extra_number = find_written_numbers(rest)
+        print(extra_number)
+        print(data)
+        for number in extra_number:
+            data.append(number[1])
+        if len(data) == 0:
+            out.append(0)
+        else:
+            print(data)
+
         match len(data):
                 case (0):
                     print("invalid input")
@@ -34,9 +76,8 @@ def solve(examples):
                         except:
                             print("error")
                     if dit < 10:
-                        lookiehere = int(str(dit)[0] + str(dit)[0])
-                        out.append(lookiehere)
-                        print("very special case" + str(lookiehere) )
+                        loe = int(str(dit)[0] + str(dit)[0])
+                        out.append(loe)
                     else:
                         out.append(dit)
     print(out)
